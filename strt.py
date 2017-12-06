fe78a6b9777345f54252a66a39cdc8004b53e172
danlizard <30766104+danlizard@users.noreply.github.com>
2017-12-06 17:23:51 +0300

    awful thing

---
+++ strt.py
@@ -0,0 +1,7 @@
+from request import *
+
+tlk = 432442820:AAGWVeJ7-L9wFqM9sUVO3rPqg47chu5R5lo
+gmk = AIzaSyA-EBqouC2zCdBfCJYPZUa89JMvPeYgc28
+
+def getInf():
+	https://api.telegram.org/bot<token>/getUpdates
